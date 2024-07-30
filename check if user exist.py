c19dda2fdc71a8019e6127fa123b03ef91822293
Decembboy <debor9130@gmail.com>
2024-07-30 20:09:28 +0530

    Add files via upload

---
+++ check if user exist.py
@@ -0,0 +1,27 @@
+li=[]
+lis=[]
+username=input("UserName: ")
+email=input("E-Mail: ")
+pswd=input("Password: ")
+li.append(username)
+li.append(email)
+li.append(pswd)
+
+
+
+enter=input("   ")
+flag=0
+liz=li[0]
+if enter==liz:
+    flag=1
+    lis.append(enter)
+if flag==0:
+    print("Not found")
+
+
+
+
+
+        
+    
+    
